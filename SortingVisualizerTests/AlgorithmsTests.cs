1a4a226e1acea1103db82b35537f06958e9b0bf6
rklusa <ryan.klusacek@hotmail.ca>
2024-06-19 14:57:57 -0400

    Added: Simple unit Tests using MSTestV2 on the sorting algorithms -- all currently running at a pass

---
+++ SortingVisualizerTests/AlgorithmsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingVisualizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingVisualizer.Tests
+{
+    [TestClass()]
+    public class AlgorithmsTests
+    {
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            // arrange
+            int[] testArray = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            // act
+            int[] sortedTestArray = Algorithms.BubbleSort(testArray);
+            // assert
+            CollectionAssert.AreEqual(testArray, sortedTestArray);
+        }
+
+        [TestMethod()]
+        public void QuickSortTest()
+        {
+            // arrange
+            int[] testArray = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            // act
+            int[] sortedTestArray = Algorithms.QuickSort(testArray, 0, testArray.Length - 1);
+            // assert
+            CollectionAssert.AreEqual(testArray, sortedTestArray);
+        }
+
+        [TestMethod()]
+        public void SelectionSortTest()
+        {
+            // arrange
+            int[] testArray = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            // act
+            int[] sortedTestArray = Algorithms.SelectionSort(testArray);
+            // assert
+            CollectionAssert.AreEqual(testArray, sortedTestArray);
+        }
+
+        [TestMethod()]
+        public void HeapSortTest()
+        {
+            // arrange
+            int[] testArray = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            // act
+            int[] sortedTestArray = Algorithms.HeapSort(testArray);
+            // assert
+            CollectionAssert.AreEqual(testArray, sortedTestArray);
+        }
+    }
+}
